c514307105278293e26e1f8fd7e082ed948e6d1f
Trkzi-Omar <81875005+Trkzi-Omar@users.noreply.github.com>
2026-06-10 16:58:46 +0100

    fix(assets): use exported app icons

--- script/package_release.sh
+++ script/package_release.sh
@@ -17,6 +17,7 @@ APP_RESOURCES="$APP_CONTENTS/Resources"
 APP_BINARY="$APP_MACOS/$APP_NAME"
 INFO_PLIST="$APP_CONTENTS/Info.plist"
 RESOURCE_SOURCE="$ROOT_DIR/Sources/CodexUsageTracker/Resources"
+DMG_RESOURCE_SOURCE="$RESOURCE_SOURCE/DMG"
 ARCHIVE_ZIP="$DIST_DIR/$APP_NAME-macOS.zip"
 ARCHIVE_DMG="$DIST_DIR/$APP_NAME-macOS.dmg"
 DMG_STAGING="$DIST_DIR/dmg-staging"
@@ -123,12 +124,25 @@ package_dmg() {
   cp -R "$APP_BUNDLE" "$DMG_STAGING/"
   ln -s /Applications "$DMG_STAGING/Applications"
 
-  hdiutil create \
-    -volname "$APP_NAME" \
-    -srcfolder "$DMG_STAGING" \
-    -ov \
-    -format UDZO \
-    "$ARCHIVE_DMG"
+  if command -v create-dmg >/dev/null 2>&1; then
+    create-dmg \
+      --volname "$APP_NAME" \
+      --volicon "$DMG_RESOURCE_SOURCE/VolumeIcon.icns" \
+      --background "$DMG_RESOURCE_SOURCE/dmg-background.png" \
+      --window-size 660 420 \
+      --icon-size 128 \
+      --icon "$APP_NAME.app" 150 216 \
+      --app-drop-link 510 216 \
+      "$ARCHIVE_DMG" \
+      "$DMG_STAGING"
+  else
+    hdiutil create \
+      -volname "$APP_NAME" \
+      -srcfolder "$DMG_STAGING" \
+      -ov \
+      -format UDZO \
+      "$ARCHIVE_DMG"
+  fi
 
   notarize_if_configured "$ARCHIVE_DMG"
 }
